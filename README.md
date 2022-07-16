6a205842bccd1e60884d50c75dc932a292672eea
MicahBird <birdsdenver@gmail.com>
2022-07-16 17:39:51 -0600

    Add dotfiles

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # My Dotfiles
 
 ![meme](meme.jpg)
+---
+This repo includes my configs for:
+- ZSH
+- Kitty
+- Alacritty
+- NeoVim
 
 ## How to install
+My dotfiles are managed with *****chezmoi**, so you just run one command to install chezmoi and apply my dotfiles
+
+`sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply MicahBird`
+
+## Installing Programs
+Included with my dotfiles is a ansible playbook that installs some packages and downloads lf, k9s, and onefetch to your .local/bin/
+
+After installing Ansible, run the ansible playbook with the following commands:
+```
+cd .setup/
+ansible-playbook -K setup.yml
+```
---
+++ dot_oh-my-zsh-custom/custom.zsh
@@ -0,0 +1,23 @@
+# Preferred editor for local and remote sessions
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nvim'
+fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+alias ll="ls -lah"
+alias icat="kitty +kitten icat"
+#Alt Key Binding
+bindkey "\e[1;3C" forward-word
+bindkey "\e[1;3D" backward-word
