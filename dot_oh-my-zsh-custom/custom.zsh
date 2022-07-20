3b85d828267053f0ade6f45c4baf57a8f972dd67
MicahBird <31116011+MicahBird@users.noreply.github.com>
2022-07-20 07:36:52 -0600

    Firefox Wayland

--- dot_oh-my-zsh-custom/custom.zsh
+++ dot_oh-my-zsh-custom/custom.zsh
@@ -5,6 +5,10 @@ else
   export EDITOR='nvim'
 fi
 
+# Firefox Wayland
+if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+    export MOZ_ENABLE_WAYLAND=1
+fi
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
 
