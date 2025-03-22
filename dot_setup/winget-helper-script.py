da26a8696efd2dd7b77a14b00da96deedcf4bda4
MicahBird <31116011+MicahBird@users.noreply.github.com>
2025-03-22 17:34:29 -0600

    Tweak helper script and add LocalSend as a package

--- dot_setup/winget-helper-script.py
+++ dot_setup/winget-helper-script.py
@@ -10,6 +10,7 @@ package_ids = [
     "Alacritty.Alacritty",
     "eloston.ungoogled-chromium",
     "Neovim.Neovim",
+    "LocalSend.LocalSend",
 ]
 
 # Template for YAML configuration
@@ -23,7 +24,18 @@ yaml_template = """
         source: winget
 """
 
+start_file = """# yaml-language-server: $schema=https://aka.ms/configuration-dsc-schema/0.2
+
+properties:
+  resources:
+"""
+end_file = """
+  configurationVersion: 0.2.0
+"""
+
 # Generate YAML for each package ID
+print(start_file, end='')
 for package_id in package_ids:
     yaml_output = yaml_template.format(id=package_id)
     print(yaml_output, end='')
+print(end_file, end='')
